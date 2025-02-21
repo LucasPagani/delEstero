1a08a398d6279932116ec2ef0dab3454403ffadb
LucasPagani <100729440+LucasPagani@users.noreply.github.com>
2025-02-21 15:33:43 +0100

    nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductFormComponent } from './components/product-form/product-form.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
-  { path: '', component: ProductListComponent },
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductListComponent },
 { path: 'product/:id', component: ProductDetailsComponent },
 { path: 'create', component: ProductFormComponent },
 { path: 'edit/:id', component: ProductFormComponent }
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA2ju8QKw2MPmzRmkyoIyQe6cgVxcI7eZ8",
-    authDomain: "projectrocio.firebaseapp.com",
-    projectId: "projectrocio",
-    storageBucket: "projectrocio.firebasestorage.app",
-    messagingSenderId: "541018058955",
-    appId: "1:541018058955:web:307e0000db0ea42f47ab4e",
-    measurementId: "G-T66YW4533F"
+    apiKey: window.env.FIREBASE_API_KEY,
+    authDomain: window.env.FIREBASE_AUTH_DOMAIN,
+    projectId: window.env.FIREBASE_PROJECT_ID,
+    storageBucket: window.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: window.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: window.env.FIREBASE_APP_ID,
+    measurementId: window.env.FIREBASE_MEASUREMENT_ID,
   }
 };
